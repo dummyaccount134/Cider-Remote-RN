4f253f3afaa7f481e46386f2ef80fefd294b4ccb
booploops <49113086+booploops@users.noreply.github.com>
2025-08-22 13:45:53 -0700

    playlist loading indicator

--- app/(tabs)/library.tsx
+++ app/(tabs)/library.tsx
@@ -1,15 +1,21 @@
 import { interact } from "@/lib/interact";
-import { getLibraryPlaylists, libraryPlaylists } from "@/lib/library";
+import { getLibraryPlaylists, libraryPlaylists, libraryPlaylistsLoading } from "@/lib/library";
 import { ItemTypes } from "@/types/search";
 import { useRouter } from "expo-router";
 import { useAtomValue } from "jotai";
+import { useEffect } from "react";
 import { ScrollView, View } from "react-native";
-import { IconButton, List, Text } from "react-native-paper";
+import { ActivityIndicator, IconButton, List, MD3Colors, Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Library() {
     const playlists = useAtomValue(libraryPlaylists);
+    const playlistsLoading = useAtomValue(libraryPlaylistsLoading);
     const router = useRouter();
+
+    useEffect(() => {
+        getLibraryPlaylists();
+    }, [])
     return (
         <ScrollView>
             <SafeAreaView >
@@ -56,11 +62,14 @@ export default function Library() {
                     padding: 16,
                     fontWeight: 'bold'
                 }} variant="headlineSmall">Playlists</Text>
-
                 <List.Section>
                     <List.Item
                         title="Refresh"
-                        left={props => <List.Icon {...props} icon="refresh" />}
+                        left={props =>
+                            playlistsLoading
+                                ? <ActivityIndicator animating={true} color={MD3Colors.primary0} />
+                                : <List.Icon {...props} icon="refresh" />
+                        }
                         onPress={() => { getLibraryPlaylists() }}
                     />
                     {playlists.map((item, idx) => <List.Item
--- lib/library.ts
+++ lib/library.ts
@@ -5,10 +5,12 @@ import { v3Turbo } from "./am-api";
 const store = getDefaultStore();
 
 export const libraryPlaylists = atom<LibraryPlaylist[]>([]);
-
+export const libraryPlaylistsLoading = atom<boolean>(false);
 
 export async function getLibraryPlaylists() {
+  store.set(libraryPlaylistsLoading, true);
   const res = await v3Turbo<LibraryPlaylist[]>("/v1/me/library/playlists");
   if(!res) return; 
   store.set(libraryPlaylists, res);
+  store.set(libraryPlaylistsLoading, false);
 }
